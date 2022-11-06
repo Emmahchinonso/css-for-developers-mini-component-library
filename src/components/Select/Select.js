23d55c71b6e4710dcabc0f0daaa3fdad4daf28b0
Emmahchinonso <emmanuelchinons9@gmail.com>
2022-11-06 18:04:10 +0100

    update select styles

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,82 +8,57 @@ import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
-  const elementRef = React.useRef();
-  const labelRef = React.useRef();
-  const selectRef = React.useRef();
-
-  function resize() {
-    const leftAndRightPadding = 34;
-    const flexGap = 24;
-    const dropDownIconWidth = 24;
-    const totalWidth = leftAndRightPadding + flexGap + dropDownIconWidth;
-    console.log("elem", elementRef.current, elementRef.current.offsetWidth);
-    labelRef.current.style.setProperty(
-      "--width",
-      `${elementRef.current.offsetWidth + totalWidth}px`
-    );
-  }
-
-  React.useEffect(() => {
-    resize();
-  }, [value]);
-
-  function activateSelect() {
-    selectRef.current.select();
-  }
 
   return (
     <>
-      <Container htmlFor="selectItems" ref={labelRef} onClick={activateSelect}>
-        <select
-          hidden
-          id="selectItems"
-          value={value}
-          onChange={onChange}
-          ref={selectRef}
-        >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Container htmlFor="selectItems">
+        <NativeSelect id="selectItems" value={value} onChange={onChange}>
           {children}
-        </select>
-        <Icon id="chevron-down" />
+        </NativeSelect>
+        <PresentationalBit>
+          <span>{displayedValue}</span>
+          <Icon id="chevron-down" />
+        </PresentationalBit>
       </Container>
-      <HelperText ref={elementRef}>{displayedValue}</HelperText>
     </>
   );
 };
 
 const Container = styled.label`
   position: relative;
+  padding: 12px 18px 12px 16px;
+  cursor: pointer;
+  border: 1px solid var(--select-border);
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  font-size: 1rem;
+  line-height: 1.1;
+`;
+
+const PresentationalBit = styled.div`
   display: inline-flex;
   justify-content: space-between;
   align-items: center;
   gap: 24px;
-  border: 1px solid var(--select-border);
-  border-radius: 8px;
-  font-size: 1rem;
-  cursor: pointer;
-  line-height: 1.1;
-  background-color: ${COLORS.transparentGray15};
-  padding: 12px 18px 12px 16px;
-  width: var(--width);
-  & select {
-    appearance: none;
-    // Additional resets for further consistency
-    background-color: transparent;
-    width: 100%;
-    color: ${COLORS.gray700};
-    border: none;
-    margin: 0;
-    font-family: inherit;
-    font-size: inherit;
-    cursor: inherit;
-    line-height: inherit;
-    outline: none;
-  }
+  color: ${COLORS.gray700};
+  font-size: inherit;
+  line-height: inherit;
 `;
 
-const HelperText = styled.span`
+const NativeSelect = styled.select`
+  appearance: none;
   position: absolute;
-  left: 999px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: red;
+  border: none;
+  margin: 0;
+  font-family: inherit;
+  cursor: inherit;
+  outline: none;
   opacity: 0;
 `;
 

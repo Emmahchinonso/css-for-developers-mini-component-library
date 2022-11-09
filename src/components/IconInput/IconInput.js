51392638b9d2c78b424df3a29a09d56f161e5b1f
Emmahchinonso <emmanuelchinons9@gmail.com>
2022-11-09 08:03:07 +0100

    add Icon input component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,89 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const {
+    gap,
+    strokeWidth,
+    iconSize,
+    fontSize,
+    lineHeight,
+    ySpacing,
+  } = sizeProps[size];
+  return (
+    <Wrapper
+      style={{
+        "--width": width + "px",
+        "--gap": gap + "px",
+        "--font-size": fontSize + "px",
+        "--lineHeight": lineHeight,
+        "--y-spacing": ySpacing + "px",
+        "--stroke-width": strokeWidth + "px",
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Icon stroke={strokeWidth} size={iconSize} id={icon} />
+      <NativeInput placeholder={placeholder} />
+    </Wrapper>
+  );
 };
+
+const sizeProps = {
+  small: {
+    strokeWidth: 1,
+    iconSize: 16,
+    fontSize: 14,
+    lineHeight: 1.143,
+    ySpacing: 4,
+    gap: 8,
+  },
+  large: {
+    strokeWidth: 2,
+    iconSize: 24,
+    fontSize: 18,
+    lineHeight: 1.17,
+    ySpacing: 7,
+    gap: 12,
+  },
+};
+
+const Wrapper = styled.label`
+  width: var(--width);
+  display: flex;
+  align-items: center;
+  gap: var(--gap);
+  border-bottom: var(--stroke-width) solid ${COLORS.black};
+  padding-block: var(--y-spacing);
+  font-size: var(--font-size);
+  line-height: var(--line-height);
+
+  &:focus-within {
+    outline: 2px dotted #212121;
+    outline: 2px auto -webkit-focus-ring-color;
+    outline-offset: 2px;
+  }
+`;
+
+const NativeInput = styled.input`
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  padding: 0;
+  background-color: transparent;
+  outline: none;
+  font-size: inherit;
+  line-height: inherit;
+  border: 0;
+  flex: 1;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
